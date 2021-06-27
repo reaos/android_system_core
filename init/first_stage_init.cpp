83c62f5ed1810a839d054903d7f0859f5299326b
Ziyang Zhou <ziyang.zhou@outlook.com>
2021-06-27 09:49:22 +0000

    ? fix first stage mount

--- init/first_stage_init.cpp
+++ init/first_stage_init.cpp
@@ -528,6 +528,7 @@ int FirstStageMain(int argc, char** argv) {
         if (!fsm) {
             fsm = CreateFirstStageMount(cmdline);
         }
+        #if 0
         if (!fsm) {
             LOG(FATAL) << "FirstStageMount not available";
         }
@@ -537,8 +538,9 @@ int FirstStageMain(int argc, char** argv) {
         }
 
         if (!fsm->DoFirstStageMount()) {
-            LOG(FATAL) << "Failed to mount required partitions early ...";
+            LOG(ERROR) << "Failed to mount required partitions early ...";
         }
+        #endif
     }
 
     struct stat new_root_info {};
